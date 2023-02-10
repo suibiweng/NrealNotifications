2f8d63dbfec8be11f4816df2155da79be883e4d7
suibiweng <suibi.weng@gmail.com>
2023-02-09 21:51:01 -0700

    0209

--- Assets/Scripts/ObjectsLayerRerange.cs
+++ Assets/Scripts/ObjectsLayerRerange.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectsLayerRerange : MonoBehaviour
+public  class ObjectsLayerRerange : MonoBehaviour
 {
+   
+    public Renderer[] allRenders;
+    Camera CenterCam,peripheralCam;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        allRenders = FindObjectsOfType<Renderer>();
+
+
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (var r in allRenders) {
+
+            bool camCenter = RendererExtensions.IsVisibleFrom(r , CenterCam);
+            bool camPer = RendererExtensions.IsVisibleFrom(r, peripheralCam);
+
+
+
+
+
+        }
+
+     
+
+
+
     }
+
+
+ 
 }
